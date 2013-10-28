082b9d541733bde2797c360fa4d8395469f638ec
luivis07 <luivis07@gmail.com>
2013-10-28 16:30:25 -0400

    started working on rowobject transform

--- ScriptLinkMaster.Tests/TransformTests/RowObjectTransformTests.cs
+++ ScriptLinkMaster.Tests/TransformTests/RowObjectTransformTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using ScriptLinkMaster.CustomEntities;
 using ScriptLinkMaster.Entities;
 using ScriptLinkMaster.Transform;
 
@@ -23,6 +24,34 @@ namespace ScriptLinkMaster.Tests.TransformTests
             rowObject.RowAction = "2";
             rowObject.RowId = "3";
             return rowObject;
+        }
+        [Test]
+        public void TransformToCustomRowObject_NonNullRowObject_ReturnsCustomRowObject()
+        {
+            var rowObject = MockBasicRowObject();
+            var transform = InitTransform();
+            var result = transform.TransformToCustomRowObject(rowObject);
+            Assert.IsInstanceOf(typeof(CustomRowObject), result);
+        }
+        [Test]
+        public void TransformToCustomRowObject_CustomRowObjectWithProperties_PropertyValuesAreEqual()
+        {
+            var transform = InitTransform();
+            var formObject = MockBasicRowObject();
+            var result = transform.TransformToCustomRowObject(formObject);
+            var expected = new object[]
+            {
+                formObject.ParentRowId,
+                formObject.RowAction,
+                formObject.RowId
+            };
+            var actual = new object[]
+            {
+                result.ParentRowId,
+                result.RowAction,
+                result.RowId
+            };
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- ScriptLinkMaster.Transform/RowObjectTransform.cs
+++ ScriptLinkMaster.Transform/RowObjectTransform.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ScriptLinkMaster.CustomEntities;
+using ScriptLinkMaster.Entities;
 
 namespace ScriptLinkMaster.Transform
 {
     public class RowObjectTransform
     {
+        public CustomRowObject TransformToCustomRowObject(RowObject rowObject)
+        {
+            var CustomRowObject = new CustomRowObject();
+            CustomRowObject.ParentRowId = rowObject.ParentRowId;
+            CustomRowObject.RowAction = rowObject.RowAction;
+            CustomRowObject.RowId = rowObject.RowId;
+            return CustomRowObject;
+        }
     }
 }
